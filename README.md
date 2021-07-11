8be2e91d94e8e150a005b3824f9e915b6f6cd644
Your Name <darki@tut.by>
2021-07-11 14:56:11 +0300

    feat:base book shop

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# aws-shop-fe
+CloudFront Distribution: https://d3o53yr68knzz0.cloudfront.net/ 
+S3-website:http://aws-task5.s3-website-us-east-1.amazonaws.com/
+
---
+++ src/constants/apiPaths.ts
@@ -0,0 +1,10 @@
+
+const API_PATHS = {
+  product: 'https://uva2ltk5p3.execute-api.us-east-1.amazonaws.com/dev/',
+  order: 'https://uva2ltk5p3.execute-api.us-east-1.amazonaws.com/dev/',
+  import: 'https://uva2ltk5p3.execute-api.us-east-1.amazonaws.com/dev/',
+  bff: 'https://uva2ltk5p3.execute-api.us-east-1.amazonaws.com/dev/',
+  cart: 'https://uva2ltk5p3.execute-api.us-east-1.amazonaws.com/dev/',
+};
+
+export default API_PATHS;
